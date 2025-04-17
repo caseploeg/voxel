8db7a5db61989c839f6be18b7b5c08d18c59a51a
Case Ploeg <case.ploeg@gmail.com>
2025-04-17 00:09:42 -0400

    add textures for the bit

---
+++ public/textures/cors_server.py
@@ -0,0 +1,12 @@
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+class CORSRequestHandler(SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', '*')
+        super().end_headers()
+
+if __name__ == '__main__':
+    HTTPServer(('0.0.0.0', 7777), CORSRequestHandler).serve_forever()
+
